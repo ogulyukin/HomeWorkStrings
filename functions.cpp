3cf63728f69d2765f95e71e47db821d552e027f8
ogulyukin <o.gulyukin@yandex.ru>
2020-10-14 08:52:18 +0200

    All functions done!

--- functions.cpp
+++ functions.cpp
@@ -42,10 +42,40 @@ double Bin2Dec(char binary[]){
     return result;
 }
 void Dec2Hex(int decimal){
-
+    int resultDiv;
+    char resultHEX[256];
+    int count = 0;
+    do{
+        resultDiv = decimal%16;
+        GetHexSymbol(resultHEX,count,resultDiv);
+        decimal /= 16;
+        count++;
+    }while (decimal >= 16);
+    GetHexSymbol(resultHEX,count,decimal);
+    resultHEX[count+1] = 0;
+    int len = GetLen(resultHEX);
+    char *finalResult = new char[len];
+    for (int i = 1; i <= len; i++){
+        finalResult[i-1] = resultHEX[len-i];
+    }
+    cout << resultHEX << endl;
+    finalResult[len] = 0;
+    cout << "Переводим из десятичного в шеснадцатеричное число: " << endl;
+    cout << finalResult << endl;
 }
 int Hex2Dec(char hex[]){
-    return 0;
+    int len = GetLen(hex);
+    double result = 0;
+    int count = 0;
+    for (int i = len-1; i >=0; i--){
+        if(hex[i] >=48 && hex[i] <=57){
+            result += int(hex[i] - 48) * pow(16, count);
+        }else{
+            result += int(hex[i] - 87) * pow(16, count);
+        }
+        count++;
+    }
+    return result;
 }
 int GetLen(const char str[]){
     int count = 0;
@@ -75,4 +105,46 @@ void PrintMenu(){
     cout << "3 - Преобразует десятичое число в HEX" << endl;
     cout << "4 - Преобразует HEX в десятичное число" << endl;
     cout << "0 - Выход" << endl;
+}
+void GetHexSymbol(char hex[], int count, int decimal){
+    if (decimal <= 9){
+        hex[count] = decimal + 48;
+    }else{
+        hex[count] = decimal + 87;
+    }
+}
+bool isHex(char str[]){
+    int len = GetLen(str);
+    for (int i = 0; i < len; i++){
+        if (str[i] < 48 || str[i] > 57){
+            if (str[i] < 97 || str[i] > 102)
+                    return false;
+        }
+    }
+    return true;
+}
+void LowerCase(char str[]){
+    int len = GetLen(str);
+    for (int i = 0; i < len; i++){
+        if (str[i] > 64 && str[i] < 91){
+            str[i] += 32;
+        }
+    }
+}
+double toNumberEq(char str[]){
+    int len = GetLen(str);
+    double result = int(str[len-1]-48);
+    for (int i = 2; i <= len; i++){
+        result += (int(str[len-i])-48)*pow(10,(i-1));
+    }
+    return result;
+}
+bool isNumber(char str[]){
+    int len = GetLen(str);
+    for (int i = 0; i < len; i++){
+        if (str[i] < 48 || str[i] > 57){
+            return false;
+        }
+    }
+    return true;
 }
--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ using namespace std;
 int main() {
     setlocale(LC_ALL,"ru");
     cout << "Программа переводит числа из одной системы счисления в другую."  << endl;
+    cout << "Программа работает ТОЛЬКО с целыми числами!!!" << endl;
     PrintMenu();
     int choise;
     const int n = 256;
@@ -20,20 +21,44 @@ int main() {
         if (!choise){
             return 0;
         }else if (choise == 1){
-            cout << "Введите десятичное число : "; cin >> decResult;
-            Dec2Bin(decResult);
+            cout << "Введите десятичное число : "; cin >> binary;
+            check = isNumber(binary);
+            if (check){
+                decResult = toNumberEq(binary);
+                Dec2Bin(decResult);
+            }else{
+                cout << "введено не число!" << endl;
+            }
         }else if (choise == 2){
                 cout << "Введите двоичное число : " << endl;
                 cin >> binary;
                 check = isBinary(binary);
-                if (!check)
+                if (check){
+                    decResult = Bin2Dec(binary);
+                    cout << "Перевдим число в десятичное : " << decResult << endl;
+                }else{
                     cout << "введено не двоичное число!" << endl;
-            decResult = Bin2Dec(binary);
-            cout << "Перевдим число в десятичное : " << decResult << endl;
+                }
         }else if (choise == 3){
-
+            cout << "Введите десятичное число : "; cin >> binary;
+            check = isNumber(binary);
+            if (check){
+                decResult = toNumberEq(binary);
+                Dec2Hex(decResult);
+            }else{
+                cout << "введено не число!" << endl;
+            }
         }else{
-
+            cout << "Введите шестнадцатиричное число : " << endl;
+            cin >> binary;
+            LowerCase(binary);
+            check = isHex(binary);
+            if (check){
+                decResult = Hex2Dec(binary);
+                cout << "Перевдим число в десятичное : " << decResult << endl;
+            }else{
+                cout << "введено не верное число!" << endl;
+            }
         }
         PrintMenu();
         choise = GetChoise();
