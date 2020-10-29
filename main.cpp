5052bbff364f439dd2c8e42fefe5e6fe2f08bc46
ogulyukin <o.gulyukin@yandex.ru>
2020-10-29 16:03:21 +0200

    a bit modifed and ready now

--- main.cpp
+++ main.cpp
@@ -1,28 +1,111 @@
 /* Гулюкин О.В.
- * Написать рекурсивную функцию которая выводит на экран числа Фибоначчи до указанного предела.
+ * Написать программу игря в крестики - нолики
  */
 #include <iostream>
-
 using namespace std;
-int CalcFibonachi(int iNumber);
-void ShowArray( int array[], int size);
-int main() {
-    setlocale(LC_ALL,"ru");
-    int iNumber;
-    cout  << "Программа выводит числа фибоначи до указанного предела."  << endl;
-    cout << "Введите предел вывода : "; cin >> iNumber;
-    for (int i = 0; i <= iNumber; i++){
-        cout  << CalcFibonachi(i) << "\t";
+void TurnMake(char board[][3]);
+void IITurnMake(char board[][3]);
+void DrawScene(char board[][3]);
+bool CheckWinner(char board[][3], char win);
+bool CheckNoWin(char board[][3]);
+int RandomNumber();
+int main()
+{
+    const int SIZE = 3;
+    char board[SIZE][SIZE] = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
+    bool winX;
+    bool winO;
+    bool noWin;
+    DrawScene(board);
+    do {
+        cout << "Ход Крестика" << endl;
+        TurnMake(board);
+        DrawScene(board);
+        winX = CheckWinner(board, 'X');
+        noWin = CheckNoWin(board);
+        if (winX || noWin)
+            break;
+        cout << "Ход Нолика" << endl;
+        IITurnMake(board);
+        DrawScene(board);
+        winO = CheckWinner(board, '0');
+        noWin = CheckNoWin(board);
+    } while (!winX && !winO && !noWin);
+    if (winX) {
+        cout << "Выиграл Крестик!" << endl;
     }
-    return 0;
+    if (winO) {
+        cout << "Выиграл Нолик!" << endl;
+    }
+    if (noWin)
+        cout << "Ничья!" << endl;
 }
-int CalcFibonachi(int iNumber) {
-
-    if (iNumber == 1){
-        return 1;
-    }else if(iNumber == 0){
-        return 0;
-    }else{
-        return (CalcFibonachi(iNumber - 1) + CalcFibonachi(iNumber -2));
+void TurnMake(char board[][3]){
+    int x,y;
+    do {
+        do {
+            cout << "Введите первую координату : "; cin >> x;
+            cout << "Введите вторую координату : "; cin >> y;
+        } while (x < 0 || y < 0 || x > 2 || y > 2);
+    } while (board[x][y] == '0' || board[x][y] == 'X');
+    board[x][y] = 'X';
+}
+void IITurnMake(char board[][3]){
+    int x,y;
+    do {
+        do {
+            x = RandomNumber();
+            y = RandomNumber();
+        } while (x < 0 || y < 0 || x > 2 || y > 2);
+    } while (board[x][y] == '0' || board[x][y] == 'X');
+    board[x][y] = '0';
+}
+void DrawScene(char board[][3]){
+    system ("clear"); //Системная команда Linux для Windows используйте cls
+    cout << "---------" << endl;
+    for (int i = 0; i <= 2; i++){
+        for (int j = 0; j < 1; j++){
+            cout << board[i][j] << " | " << board[i][j + 1] << " | " << board[i][j +2] << endl;
+            cout << "---------" << endl;
+        }
+        cout << endl;
     }
+}
+int RandomNumber(){
+    system ("sleep 1"); // Системенная команда Linux для Windows используйте pause 1
+    srand(time(nullptr));
+    int res = rand()%3;
+    return res;
+}
+bool CheckWinner(char board[][3], char win){
+    bool winXO = false;
+    for (int i = 0; i <= 2; i++) {
+        for (int j = 0; j < 1; j++) {
+            if (board[i][j] == win && board[i][j+1] == win && board[i][j+2] == win) {
+                winXO = true;
+                break;
+            }
+            if (board[j][i] == win && board[j + 1][i] == win && board[j + 2][i] == win) {
+                winXO = true;
+                break;
+            }
+        }
+    }
+    if (board [0][0] == win && board [1][1] == win && board [2][2] == win){
+        winXO = true;
+    }
+    if (board [2][0] == win && board [1][1] == win && board [0][2] == win){
+        winXO = true;
+    }
+    return winXO;
+}
+bool CheckNoWin(char board[][3]){
+    bool noWin = true;
+    for (int i = 0; i <= 2; i++) {
+        for (int j = 0; j <= 2; j++) {
+            if (board[i][j] == ' ')
+                noWin = false;
+        }
+    }
+    return noWin;
 }
